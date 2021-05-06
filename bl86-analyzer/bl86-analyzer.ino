3678fa2e5804037b77837805c2403082b5d72645
Tore Sinding Bekkedal <toresbe@protonmail.com>
2021-05-06 18:03:56 +0200

    working bl86 analyzer

--- bl86-analyzer/bl86-analyzer.ino
+++ bl86-analyzer/bl86-analyzer.ino
@@ -13,14 +13,14 @@ int dataPin = 2;
 #define MCL_STX 5
 
 
-#define BUFFER_SIZE 128
+#define BUFFER_SIZE 1024
 volatile int str_p = 0;
 char str_buf[BUFFER_SIZE];
 
 int val_i = 0;
 int val_p = 0;
-int val_buf[BUFFER_SIZE];
-int * stx_at = 0;
+uint8_t val_buf[BUFFER_SIZE];
+uint8_t * stx_at = 0;
 
 
 
@@ -46,7 +46,7 @@ void setup() {
   last_change = micros();
 
   // Setup timer and interrupt
-  attachInterrupt(digitalPinToInterrupt(dataPin), pinChange, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(dataPin), pinChange, RISING);
 
 
 }
@@ -55,10 +55,11 @@ void loop(void) {
 
 }
 
-void dump_message(char * out_buf, int * start, int * end) {
-  int * p = start + 1;
+
+void dump_message(uint8_t * start, uint8_t * end) {
+  uint8_t * p = start;
   Serial.write("STX ");
-  while(p != end) {
+  while (p < end) {
     Serial.print(*p, HEX);
     Serial.write(" ");
     p++;
@@ -68,52 +69,48 @@ void dump_message(char * out_buf, int * start, int * end) {
   Serial.flush();
 }
 volatile int processing_message = 0;
+uint8_t incoming_byte = 0;
+int bits_received = 0;
 
 void pinChange(void) {
+  // Beolink signals are spaced by timing units of 3.125 ms.
+  // t1 ... t5 is thus 1 ... 5 timeslots; 1 .. 3 is data,
+  // 5 is the start bit, 4 is the stop bit.
+
+  int beolink_signal = 1 + ((micros() - last_change) / 3125);
+  last_change = micros();
+  if (beolink_signal > MCL_STX) return;
+  //Serial.print(beolink_signal);
+  //Serial.write(beolink_signal == MCL_ETX ? "\n" : " ");
+
   if (processing_message) {
-    Serial.write("OVERFLOW");
-  }
-
-  if (digitalRead(dataPin) == HIGH) {
-    last_change = micros();
-  } else {
-    // Beolink signals are spaced by timing units of 3.125 ms.
-    // t1 ... t5 is thus 1 ... 5 timeslots; 1 .. 3 is data,
-    // 5 is the start bit, 4 is the stop bit.
-
-    int beolink_signal = 1 + ((micros() - last_change) / 3125);
-
-    val_buf[val_p] = beolink_signal;
-
     switch (beolink_signal) {
-      case MCL_STX:
-        // Note that a message start was observed at val_buf[val_p].
-        stx_at = val_buf + val_p;
-        break;
-      case MCL_ETX:
-        processing_message = 1;
-        decode_message(str_buf, stx_at, val_buf + val_p);
-        val_p = 0; processing_message = 0;
-        break;
       case MCL_DATA_T1:
         if (!last_decoded_bit) {
           Serial.write("Decode error; last_bit = 0 and signal = t1");
-        } else {
-          val_buf[val_p] = last_decoded_bit = 0;
         }
+        last_decoded_bit = 0;
         break;
       case MCL_DATA_T2:
-        val_buf[val_p] = last_decoded_bit = last_decoded_bit ? 1 : 0;
+        last_decoded_bit = last_decoded_bit ? 1 : 0;
         break;
       case MCL_DATA_T3:
         if (last_decoded_bit) {
           Serial.write("Decode error; last_bit = 1 and signal = t3");
-        } else {
-          val_buf[val_p] = last_decoded_bit = 1;
         }
+        last_decoded_bit = 1;
+        break;
+      case MCL_ETX:
+        dump_message(stx_at, val_buf + val_p);
+        val_p = 0; processing_message = 0;
         break;
     }
+    
+    val_buf[val_p++] = last_decoded_bit;
 
-    val_p++;
+  } else if (beolink_signal == MCL_STX) {
+    // Note that a message start was observed at val_buf[val_p].
+    stx_at = val_buf + val_p;
+    last_decoded_bit = processing_message = 1;
   }
 }
